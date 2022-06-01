28aaeef9caaa7e9e312c21972d45e349082b6cc4
Лана Константинова <lnk.konst@gmail.com>
2022-06-01 22:51:00 +0300

    create gcd game

---
+++ bin/brain-gcd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import GCDGame from '../src/games/gcd.js';
+
+GCDGame();
---
+++ src/games/gcd.js
@@ -0,0 +1,30 @@
+import * as gameLines from '../index.js';
+
+const randomInt = () => Math.floor(Math.random() * 100);
+
+const div = (a, b) => {
+  let ans = 1;
+  let n = 1;
+  while (n < a && n < b) {
+    if (a % (n + 1) === 0 && b % (n + 1) === 0) ans = n + 1;
+    n += 1;
+  }
+
+  return ans;
+};
+
+export default () => {
+  const name = gameLines.greeting();
+  let i = 0;
+
+  while (i < 3) {
+    const a = randomInt();
+    const b = randomInt();
+    console.log(`Question: ${a} ${b}`);
+    const ans = div(a, b);
+
+    i = gameLines.check(ans, name, i);
+  }
+
+  console.log(`Congratulations, ${name}!`);
+};

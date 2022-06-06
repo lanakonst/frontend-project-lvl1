fc4b4295164d2870a3d9cd5b11c44fe14d6cee0d
Лана Константинова <lnk.konst@gmail.com>
2022-06-06 15:09:59 +0300

    update gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -13,8 +13,8 @@ const div = (a, b) => {
 };
 
 const getQnAns = () => {
-  const a = randomInt(100, 0);
-  const b = randomInt(100, 0);
+  const a = randomInt(100, 1);
+  const b = randomInt(100, 1);
   const ans = div(a, b);
 
   return [`${a} ${b}`, ans];
